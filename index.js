7316179fd85ad702763dd9bd7898b14311ce0e9f
Christopher Andrew Kemur <christopher.kemur@gmail.com>
2020-08-30 16:53:54 +0700

    Initial Commit
    Adding index.html, index.js and style.css

---
+++ index.js
@@ -0,0 +1,57 @@
+var entryTitle = document.getElementById("entry-title");
+var entry = document.querySelector(".entry");
+var submitButton = document.getElementById("submit-button");
+var entries = document.getElementById("entries");
+var entriesNumber = document.getElementById("entries-number")
+var entryNumber = 1;
+
+
+function submitEntry(event) {
+    event.preventDefault();
+    console.log("event prevented");
+    console.log(entryTitle.value);
+
+
+    var createEntry = document.createElement("div");
+    createEntry.className = "entry-text";
+    var createTitle = document.createElement("h1");
+    createTitle.className = "post-title";
+    createTitle.innerText = entryTitle.value;
+    createEntry.appendChild(createTitle);
+    var createParagraph = document.createElement("p");
+    createParagraph.className = "entry-paragraph";
+    createParagraph.innerText = entry.value;
+    createEntry.appendChild(createParagraph);
+    createEntry.style.display = "none";
+    entries.appendChild(createEntry);
+
+
+
+    entryTitle.value = " ";
+    entry.value = " ";
+
+    var createPostNumber = document.createElement("button");
+    createPostNumber.className = "entry-number";
+    createPostNumber.innerText = entryNumber;
+    entriesNumber.appendChild(createPostNumber);
+
+    createPostNumber.addEventListener("click", function() {
+        var allEntries = document.querySelectorAll(".entry-text");
+        console.log(allEntries)
+        for (var i = 0; i < allEntries.length; i++) {
+            allEntries[i].style.display = "none";
+        }
+        createEntry.style.display = "block";
+    });
+
+
+
+    entryNumber++;
+
+
+
+
+
+}
+
+submitButton.addEventListener("click", submitEntry);
